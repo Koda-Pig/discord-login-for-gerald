b124cc8d7fc99533988e6452c6851fcb281f78dc
Josh Koter <josh.koter@gmail.com>
2024-12-19 17:23:31 +0200

    ...

--- components/discord-login-form.tsx
+++ components/discord-login-form.tsx
@@ -9,18 +9,16 @@ import { SignInButton } from "./sign-in-button";
 
 export function DiscordLoginForm() {
   return (
-    <div className="flex flex-col gap-6">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Login with Discord using the button below:
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <SignInButton />
-        </CardContent>
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Login with Discord using the button below:
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SignInButton />
+      </CardContent>
+    </Card>
   );
 }

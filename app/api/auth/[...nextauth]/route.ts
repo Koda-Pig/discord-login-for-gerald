f90cc7f957e5b4eb891e5235e756634b57db1543
Josh Koter <josh.koter@gmail.com>
2024-12-19 11:38:43 +0200

    fix redirect uri issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,12 @@ const handler = NextAuth({
   providers: [
     Discord({
       clientId: process.env.DISCORD_APPLICATION_ID as string,
-      clientSecret: process.env.DISCORD_API_TOKEN as string
+      clientSecret: process.env.DISCORD_API_TOKEN as string,
+      authorization: {
+        params: {
+          redirect_uri: "http://localhost:3000/user"
+        }
+      }
     })
   ],
   secret: process.env.NEXTAUTH_SECRET

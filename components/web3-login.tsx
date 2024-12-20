8c0dd7ac062b5a8058d46666cfeadc9edfb99d33
Josh Koter <josh.koter@gmail.com>
2024-12-20 21:33:39 +0200

    ...

--- components/web3-login.tsx
+++ components/web3-login.tsx
@@ -1,10 +1,11 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import styles from "./web3-login-styles.module.css";
 
 export function Web3Login() {
   return (
-    <div className="my-6">
+    <div className={styles.container} id="#web3-rainbow-container">
       <ConnectButton />
     </div>
   );

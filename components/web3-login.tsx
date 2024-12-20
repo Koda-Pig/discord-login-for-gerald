dd8bb10b0bbbde4af68e2c9b79cc60d323248a4f
Josh Koter <josh.koter@gmail.com>
2024-12-20 21:39:39 +0200

    ...

--- components/web3-login.tsx
+++ components/web3-login.tsx
@@ -6,7 +6,7 @@ import styles from "./web3-login-styles.module.css";
 export function Web3Login() {
   return (
     <div className={styles.container} id="#web3-rainbow-container">
-      <ConnectButton />
+      <ConnectButton label="Connect wallet" />
     </div>
   );
 }

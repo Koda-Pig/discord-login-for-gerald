9ee52ab33d371bae3b8afc2e29b00ee18b1d5e1a
Josh Koter <josh.koter@gmail.com>
2024-12-23 17:44:23 +0200

    ...

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -13,10 +13,21 @@ const handler = NextAuth({
       }
     })
   ],
-  debug: true,
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/signIn"
+  },
+  debug: true,
+  logger: {
+    error(code, ...message) {
+      console.error('[next-auth][error]', { code, message });
+    },
+    warn(code, ...message) {
+      console.warn('[next-auth][warn]', { code, message });
+    },
+    debug(code, ...message) {
+      console.log('[next-auth][debug]', { code, message });
+    },
   }
 });
 
